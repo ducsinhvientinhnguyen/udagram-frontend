26e49ba273cf6d31357cf06f3db94014bb09e72d
ducsinhvientinhnguyen <ducsinhvientinhnguyen@gmail.com>
2024-04-20 09:44:23 +0700

    Update env url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "http://a161387bbb5524494be7127d51afdb95-397807564.us-east-2.elb.amazonaws.com:8080/api/v0",
 };
 
 /*
